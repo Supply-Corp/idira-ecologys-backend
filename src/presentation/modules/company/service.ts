55396648a0ddc712ceeaa1cf0f1766ce3f216f97
zandrij <ajosuesh@gmail.com>
2024-02-27 10:53:35 -0500

    feat: create company service

--- src/presentation/modules/company/service.ts
+++ src/presentation/modules/company/service.ts
@@ -1,2 +1,74 @@
+import { prisma } from "../../../configuration";
+import { CreateCompanyDto, CustomError } from "../../../domain";
 
-export class CompanyService {}
+export class CompanyService {
+
+  async create(dto: CreateCompanyDto) {
+    const {
+        name,
+        email,
+        razon_social,
+        ruc,
+        distrito,
+        provincia,
+        address,
+
+        name_representative,
+        dni_representative,
+        email_representative,
+
+        name_general_manager,
+        dni_general_manager,
+        email_general_manager,
+
+        name_supervisor,
+        dni_supervisor,
+        email_supervisor
+    } = dto;
+
+    const company = await prisma.company.create({
+      data: {
+        name,
+        email,
+        razon_social,
+        ruc,
+        distrito,
+        provincia,
+        address,
+      },
+    });
+    if( !company ) throw CustomError.badRequest('Error al crear la empresa');
+
+    await prisma.representative.create({
+      data: {
+        name: name_representative,
+        dni: dni_representative,
+        email: email_representative ? email_representative : null,
+        companyId: company.id,
+      },
+    });
+
+    await prisma.generalManager.create({
+      data: {
+        name: name_general_manager,
+        dni: dni_general_manager,
+        email: email_general_manager ? email_general_manager : null,
+        companyId: company.id,
+      },
+    });
+
+    await prisma.supervisor.create({
+      data: {
+        name: name_supervisor,
+        dni: dni_supervisor,
+        email: email_supervisor ? email_supervisor : null,
+        companyId: company.id,
+      },
+    });
+    
+    return {
+      msg: 'Empresa registrada con éxito'
+    };
+  }
+
+}
