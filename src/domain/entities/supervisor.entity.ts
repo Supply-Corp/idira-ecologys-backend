1adcbe70c6716d49bbe9b4cea7c5981b075e0cd9
zandrij <ajosuesh@gmail.com>
2024-02-27 09:45:56 -0500

    feat: supervisor entity

---
+++ src/domain/entities/supervisor.entity.ts
@@ -0,0 +1,40 @@
+
+export class SupervisorEntity {
+
+    constructor(
+        public readonly id: number,
+        public readonly name: string,
+        public readonly email: string,
+        public readonly dni: string,
+        
+        public readonly createdAt?: Date,
+        public readonly updatedAt?: Date
+    ) {}
+
+    static fromObject(object: { [key: string]: any }) {
+
+        const { 
+            id, 
+            name, 
+            email, 
+            dni,
+            createdAt,
+            updatedAt
+        } = object;
+
+        if (!id) throw "missing id";
+        if (!name) throw "missing name";
+        if (!email) throw "missing email";
+        if (!dni) throw "missing dni";
+
+        return new SupervisorEntity(
+            id,
+            name,
+            email,
+            dni,
+            updatedAt,
+            createdAt
+        );
+    }
+
+}
