ba56a8cbe51079b6f74bde1c98e707b7eaa04735
zandrij <ajosuesh@gmail.com>
2024-02-27 12:08:35 -0500

    feat: create middleware trim

---
+++ src/presentation/middlewares/trim.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+export class TrimMiddleware {
+
+    static trim = (req: Request, res: Response, next: NextFunction) => {
+        for (const key in req.body) {
+            if (typeof req.body[key] === 'string') {
+                req.body[key] = req.body[key].trim();
+            }
+        }
+        next();
+    };
+
+}
