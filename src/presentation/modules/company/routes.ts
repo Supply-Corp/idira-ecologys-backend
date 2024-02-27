f1fa4d01182c78370e8eb797ea46a6942dc6b922
zandrij <ajosuesh@gmail.com>
2024-02-27 14:53:05 -0500

    feat: get and list company

--- src/presentation/modules/company/routes.ts
+++ src/presentation/modules/company/routes.ts
@@ -9,9 +9,11 @@ export class CompanyRoutes {
         const service = new CompanyService();
         const controller = new CompanyController( service );
 
+        router.get('/', controller.list);
         router.post('/', controller.store);
         router.put('/:id', controller.update);
         router.delete('/:id', controller.delete);
+        router.get('/:id', controller.get);
 
         return router;
     }
