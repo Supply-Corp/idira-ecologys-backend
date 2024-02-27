8d9b484ed3e3f4625e32ed8ad630a927bd19e2e9
zandrij <ajosuesh@gmail.com>
2024-02-27 10:53:13 -0500

    feat: create company controller

--- src/presentation/modules/company/controller.ts
+++ src/presentation/modules/company/controller.ts
@@ -1,2 +1,20 @@
+import { Request, Response } from "express";
+import { CompanyService } from "./service";
+import { CreateCompanyDto, HandleError } from "../../../domain";
 
-export class CompanyController {}
+export class CompanyController {
+
+    constructor(
+        private service: CompanyService
+    ) {}
+
+    store = async (req: Request, res: Response) => {
+        const [error, dto] = await CreateCompanyDto.create(req);
+        if (error) return res.status(400).json({ error });
+
+        this.service.create( dto! )
+        .then((response) => res.send(response))
+        .catch((error) => HandleError.handle(error, res));
+    }
+
+}
