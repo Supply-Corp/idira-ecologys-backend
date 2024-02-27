1d6dbf96b86b3c44163d8ff027be20a1fee22c9b
zandrij <ajosuesh@gmail.com>
2024-02-27 10:28:15 -0500

    feat: add route create company

--- src/presentation/modules/company/routes.ts
+++ src/presentation/modules/company/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import { CompanyController } from "./controller";
+import { CompanyService } from "./service";
 
 export class CompanyRoutes {
     static get routes(): Router {
         const router = Router();
+
+        const service = new CompanyService();
+        const controller = new CompanyController( service );
+
+        router.post('/', controller.store);
 
         return router;
     }
