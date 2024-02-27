d3bc5aa7c37a48ceaea34e25df5927cc0c52b4a3
zandrij <ajosuesh@gmail.com>
2024-02-27 14:50:59 -0500

    feat: get company dto

---
+++ src/domain/dtos/company/get.dto.ts
@@ -0,0 +1,36 @@
+import { Schema } from "express-validator";
+import { FieldQuery } from "../../utils/field-query.util";
+import { Request } from "express";
+import { FieldValidation } from "../../utils/field-validation.util";
+
+export class GetCompanyDto {
+
+    constructor(
+        public readonly id: number,
+    ) {}
+
+    private static schema: Schema = {
+        id: {
+            notEmpty: {
+                bail: true,
+                errorMessage: 'Id es requerido'
+            },
+            isNumeric: {
+                bail: true,
+                errorMessage: 'Id no es un número'
+            }
+        },
+    };
+
+    static async create(req: Request): Promise<[FieldQuery[]?, GetCompanyDto?]> {
+
+        const id = req.params.id;
+
+        try {
+            const valid = await FieldValidation.validate(this.schema, req);
+            return [valid, undefined];
+        } catch (error) {
+            return [undefined, new GetCompanyDto( +id )];
+        }
+    }
+}
