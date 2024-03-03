cf67ebef748478ca6539c6428dce63f9ce8afaca
zandrij <ajosuesh@gmail.com>
2024-03-03 11:04:55 -0500

    feat: get sede dto

--- src/domain/dtos/sedes/get.dto.ts
+++ src/domain/dtos/sedes/get.dto.ts
@@ -1,17 +1,38 @@
 import { Schema } from "express-validator";
 import { FieldQuery } from "../../utils/field-query.util";
+import { FieldValidation } from "../../utils/field-validation.util";
+import { Request } from "express";
 
 
 export class GetSedeDto {
 
-    constructor() {
-        
+    constructor(
+        public readonly id: number
+    ) {}
+
+    private static schema: Schema = {
+        id: {
+            trim: true,
+            notEmpty: {
+                bail: true,
+                errorMessage: 'Id es requerido'
+            },
+            isNumeric: {
+                bail: true,
+                errorMessage: 'Id no es numérico'
+            }
+        }
     }
 
-    private schema: Schema = {
+    static async create(req: Request): Promise<[FieldQuery[]?, GetSedeDto?]> {
+        const id = req.params.id;
 
+        try {
+            const valid = await FieldValidation.validate(this.schema, req);
+            return [valid, undefined];
+        } catch (error) {
+            return [undefined, new GetSedeDto(+id)];
+        }
     }
-
-    static async create(req: Request): Promise<[FieldQuery[]?, GetSedeDto?]> {}
 
 }
