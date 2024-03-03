6497c75ce5eca6f55e47cdc8acc2423c1ff7f5a8
zandrij <ajosuesh@gmail.com>
2024-03-03 11:21:14 -0500

    feat: add sede routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthRoutes } from "./modules/auth";
 import { CompanyRoutes } from "./modules/company";
 import { TrimMiddleware } from "./middlewares";
+import { SedesRoutes } from "./modules/sedes";
 
 export class ServerRoutes {
     static get routes(): Router {
@@ -11,6 +12,7 @@ export class ServerRoutes {
         router.use(TrimMiddleware.trim);
         router.use('/api/v1/auth', AuthRoutes.routes);
         router.use('/api/v1/company', CompanyRoutes.routes);
+        router.use('/api/v1/sede', SedesRoutes.routes);
 
         return router;
     }
