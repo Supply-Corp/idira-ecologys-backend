8b4671dcef4515559c9b71332af0295218fabbf6
zandrij <ajosuesh@gmail.com>
2024-03-03 12:25:29 -0500

    refactor: order code

--- src/presentation/modules/sedes/service.ts
+++ src/presentation/modules/sedes/service.ts
@@ -11,140 +11,140 @@ import {
 } from "../../../domain";
 
 export class SedesService {
-  async store(dto: CreateSedeDto) {
-    try {
-        const company = await prisma.company.findFirst({
-            where: { id: dto.companyId },
-        });
+    async store(dto: CreateSedeDto) {
+        try {
+            const company = await prisma.company.findFirst({
+                where: { id: dto.companyId },
+            });
 
-        if (!company)
-            throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
-        if (company.state === "DELETE")
-            throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
+            if (!company)
+                throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
+            if (company.state === "DELETE")
+                throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
 
-        const create = await prisma.sedes.create({
-            data: { ...dto },
-        });
+            const create = await prisma.sedes.create({
+                data: { ...dto },
+            });
 
-        const sede = SedeEntity.fromObject(create);
+            const sede = SedeEntity.fromObject(create);
 
-        return { ...sede };
-    } catch (error) {
+            return { ...sede };
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async update(dto: UpdateSedeDto) {
+        try {
+            const company = await prisma.company.findFirst({
+                where: { id: dto.companyId },
+            });
+
+            if (!company)
+                throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
+            if (company.state === "DELETE")
+                throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
+
+            const update = await prisma.sedes.update({
+                data: { ...dto },
+                where: { id: dto.id },
+            });
+
+            const sede = SedeEntity.fromObject(update);
+
+            return { ...sede };
+        } catch (error) {
+            throw `${error}`;
+        }
+    }
+
+    async delete(dto: DeleteSedeDto) {
+        try {
+            const search = await prisma.sedes.findFirst({
+                where: { id: dto.id },
+            });
+
+            if (!search) throw CustomError.notFound(`Sede ${dto.id} no existe`);
+            if (search.state === "DELETE")
+                throw CustomError.notFound(`Sede ${dto.id} no existe`);
+
+            const deleted = await prisma.sedes.update({
+                where: { id: dto.id },
+                data: { state: "DELETE" },
+            });
+
+            const sede = SedeEntity.fromObject(deleted);
+
+            return { ...sede };
+        } catch (error) {
         throw error;
+        }
     }
-  }
 
-  async update(dto: UpdateSedeDto) {
-    try {
-        const company = await prisma.company.findFirst({
-            where: { id: dto.companyId },
-        });
+    async get(dto: GetSedeDto) {
+        try {
+            const find = await prisma.sedes.findFirst({
+                where: { id: dto.id },
+                include: { Company: true },
+            });
 
-        if (!company)
-            throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
-        if (company.state === "DELETE")
-            throw CustomError.badRequest(`No existe la empresa ${dto.companyId}`);
+            if (!find) throw CustomError.badRequest(`No existe la sede ${dto.id}`);
+            if (find.state === "DELETE")
+                throw CustomError.badRequest(`No existe la sede ${dto.id}`);
 
-        const update = await prisma.sedes.update({
-            data: { ...dto },
-            where: { id: dto.id },
-        });
+            const sede = SedeEntity.fromObject(find);
 
-        const sede = SedeEntity.fromObject(update);
-
-        return { ...sede };
-    } catch (error) {
-        throw `${error}`;
+            return { ...sede };
+        } catch (error) {
+        throw error;
+        }
     }
-  }
 
-  async delete(dto: DeleteSedeDto) {
-    try {
-        const search = await prisma.sedes.findFirst({
-            where: { id: dto.id },
-        });
+    async list(dto: PaginationDto) {
+        const { page, limit, search } = dto;
 
-        if (!search) throw CustomError.notFound(`Sede ${dto.id} no existe`);
-        if (search.state === "DELETE")
-            throw CustomError.notFound(`Sede ${dto.id} no existe`);
-
-        const deleted = await prisma.sedes.update({
-            where: { id: dto.id },
-            data: { state: "DELETE" },
-        });
-
-        const sede = SedeEntity.fromObject(deleted);
-
-        return { ...sede };
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async get(dto: GetSedeDto) {
-    try {
-        const find = await prisma.sedes.findFirst({
-            where: { id: dto.id },
-            include: { Company: true },
-        });
-
-        if (!find) throw CustomError.badRequest(`No existe la sede ${dto.id}`);
-        if (find.state === "DELETE")
-            throw CustomError.badRequest(`No existe la sede ${dto.id}`);
-
-        const sede = SedeEntity.fromObject(find);
-
-        return { ...sede };
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async list(dto: PaginationDto) {
-    const { page, limit, search } = dto;
-
-    try {
-      const [results, total] = await Promise.all([
-        prisma.sedes.findMany({
-          skip: (page - 1) * limit,
-          take: limit,
-          where: {
-            state: "ACTIVE",
-            name: {
-              startsWith: `%${search}%`,
+        try {
+        const [results, total] = await Promise.all([
+            prisma.sedes.findMany({
+            skip: (page - 1) * limit,
+            take: limit,
+            where: {
+                state: "ACTIVE",
+                name: {
+                startsWith: `%${search}%`,
+                },
             },
-          },
-          include: {
-            Company: true,
-          },
-          orderBy: { id: "desc" },
-        }),
-        prisma.sedes.count({
-          where: {
-            state: "ACTIVE",
-            name: {
-              startsWith: `%${search}%`,
+            include: {
+                Company: true,
             },
-          },
-        }),
-      ]);
+            orderBy: { id: "desc" },
+            }),
+            prisma.sedes.count({
+            where: {
+                state: "ACTIVE",
+                name: {
+                startsWith: `%${search}%`,
+                },
+            },
+            }),
+        ]);
 
-      const data = results.map(SedeEntity.fromObject);
+        const data = results.map(SedeEntity.fromObject);
 
-      const pagination = PaginationGenerate.create({
-        page,
-        limit,
-        total,
-        search,
-        url: "company",
-        results: data,
-      });
+        const pagination = PaginationGenerate.create({
+            page,
+            limit,
+            total,
+            search,
+            url: "company",
+            results: data,
+        });
 
-      return {
-        ...pagination,
-      };
-    } catch (error) {
-      throw error;
+        return {
+            ...pagination,
+        };
+        } catch (error) {
+        throw error;
+        }
     }
-  }
 }
