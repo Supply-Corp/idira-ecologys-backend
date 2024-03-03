7de9476a4fd7ecbf771bcd24ca070e7a3e20e893
zandrij <ajosuesh@gmail.com>
2024-03-03 11:21:34 -0500

    feat: create sede service

--- src/presentation/modules/sedes/service.ts
+++ src/presentation/modules/sedes/service.ts
@@ -1,9 +1,34 @@
-import { CreateSedeDto, DeleteSedeDto, GetSedeDto, PaginationDto, UpdateSedeDto } from "../../../domain";
+import { prisma } from "../../../configuration";
+import { CreateSedeDto, CustomError, DeleteSedeDto, GetSedeDto, PaginationDto, SedeEntity, UpdateSedeDto } from "../../../domain";
 
 export class SedesService {
 
-    async store(dto: CreateSedeDto) {}
-    async update(dto: UpdateSedeDto) {}
+    async store(dto: CreateSedeDto) {
+
+        try {
+            const company = await prisma.company.findFirst({
+                where: { id: dto.companyId }
+            });
+
+            if(!company) throw CustomError.badRequest(`No existe la empresa ${ dto.companyId }`);
+            if(company.state === 'DELETE') throw CustomError.badRequest(`No existe la empresa ${ dto.companyId }`);
+
+            const create = await prisma.sedes.create({
+                data: { ...dto }
+            });
+            
+            const sede = SedeEntity.fromObject(create);
+
+            return { ...sede };
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async update(dto: UpdateSedeDto) {
+
+    }
+    
     async delete(dto: DeleteSedeDto) {}
     async get(dto: GetSedeDto) {}
     async list(dto: PaginationDto) {}
