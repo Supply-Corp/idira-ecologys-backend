0506713fb2863bb6306274f473d54bf26422de23
zandrij <ajosuesh@gmail.com>
2024-03-03 11:04:16 -0500

    feat: create sede dto

--- src/domain/dtos/sedes/create.dto.ts
+++ src/domain/dtos/sedes/create.dto.ts
@@ -1,16 +1,77 @@
 import { Request } from "express";
 import { Schema } from "express-validator";
 import { FieldQuery } from "../../utils/field-query.util";
+import { FieldValidation } from "../../utils/field-validation.util";
 
 export class CreateSedeDto {
 
-    constructor() {
+    constructor(
+        public readonly name: string,
+        public readonly address: string,
+        public readonly email: string,
+        public readonly companyId: number
+    ) {}
 
+    private static schema: Schema = {
+        name: {
+            trim: true,
+            notEmpty: {
+                bail: true,
+                errorMessage: 'Nombre es requerido'
+            }
+        },
+        address: {
+            trim: true,
+            notEmpty: {
+                bail: true,
+                errorMessage: 'Dirección es requerido'
+            }
+        },
+        email: {
+            trim: true,
+            notEmpty: {
+                bail: true,
+                errorMessage: 'Email es requerido'
+            },
+            isEmail: {
+                bail: true,
+                errorMessage: 'Email es inválido'
+            }
+        },
+        companyId: {
+            trim: true,
+            notEmpty: {
+                bail:true,
+                errorMessage: 'Id de empresa es requerido'
+            },
+            isNumeric: {
+                bail: true,
+                errorMessage: 'Id de empresa no es un número'
+            }
+        }
     }
 
-    private schema: Schema = {
+    static async create(req: Request): Promise<[FieldQuery[]?, CreateSedeDto?]> {
+        const {
+            name,
+            address,
+            email,
+            companyId
+        } = req.body;
 
+        try {
+            const valid = await FieldValidation.validate(this.schema, req);
+            return [valid, undefined];
+        } catch (error) {
+            return [
+                undefined,
+                new CreateSedeDto(
+                    name,
+                    address,
+                    email,
+                    companyId
+                ),
+            ];
+        }
     }
-
-    static async create(req: Request): Promise<[FieldQuery[]?, CreateSedeDto?]> {}
 }
