a502de0ceef6e600a10d6843d84842db1a716f26
zandrij <ajosuesh@gmail.com>
2024-03-03 11:05:11 -0500

    feat: update sede dto

--- src/domain/dtos/sedes/update.dto.ts
+++ src/domain/dtos/sedes/update.dto.ts
@@ -1,16 +1,47 @@
 import { Schema } from "express-validator";
 import { FieldQuery } from "../../utils/field-query.util";
+import { FieldValidation } from "../../utils/field-validation.util";
+import { Request } from "express";
 
 export class UpdateSedeDto {
 
-    constructor() {
-        
+    constructor(
+        public readonly id: number,
+        public readonly name: string,
+        public readonly address: string,
+        public readonly email: string,
+        public readonly companyId: number
+    ) {}
+
+    private static schema: Schema = {
+
     }
 
-    private schema: Schema = {
+    static async create(req: Request): Promise<[FieldQuery[]?, UpdateSedeDto?]> {
+        const {
+            name,
+            address,
+            email,
+            companyId
+        } = req.body;
 
+        const id = req.params.id;
+
+        try {
+            const valid = await FieldValidation.validate(this.schema, req);
+            return [valid, undefined];
+        } catch (error) {
+            return [
+                undefined,
+                new UpdateSedeDto(
+                    +id,
+                    name,
+                    address,
+                    email,
+                    companyId
+                ),
+            ];
+        }
     }
-
-    static async create(req: Request): Promise<[FieldQuery[]?, UpdateSedeDto?]> {}
 
 }
