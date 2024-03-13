6e67da4adba7b7930d9c7892fef670b16ffd5af5
zandrij <ajosuesh@gmail.com>
2024-03-13 12:50:47 -0500

    feat: creates routes users (crud)

--- src/presentation/modules/users/routes.ts
+++ src/presentation/modules/users/routes.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import { UserController } from "./controller";
+import { UserServices } from "./service";
 
 export class UserRoutes {
     static get routes(): Router {
         const router = Router();
+
+        const service = new UserServices();
+        const controller = new UserController(service);
+
+        router.get("/", controller.list);
+        router.post("/", controller.store);
+        router.put("/:id", controller.update);
+        router.delete("/:id", controller.delete);
+        router.get("/:id", controller.get);
 
         return router;
     }
