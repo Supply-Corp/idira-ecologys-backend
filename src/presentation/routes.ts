20e4175f0dd01ae8edbd081d3ba85e47d2872553
zandrij <ajosuesh@gmail.com>
2024-03-16 18:47:06 -0500

    feat: add routes directories routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -4,6 +4,9 @@ import { CompanyRoutes } from "./modules/company";
 import { TrimMiddleware } from "./middlewares";
 import { SedesRoutes } from "./modules/sedes";
 import { UserRoutes } from "./modules/users";
+import { DirectoryRoutes } from "./modules/directory";
+import { SubDirectoryRoutes } from "./modules/subDirectory";
+import { SubDirectoryYearRoutes } from "./modules/subDirectoryYears";
 
 export class ServerRoutes {
     static get routes(): Router {
@@ -15,6 +18,9 @@ export class ServerRoutes {
         router.use('/api/v1/company', CompanyRoutes.routes);
         router.use('/api/v1/sede', SedesRoutes.routes);
         router.use('/api/v1/users', UserRoutes.routes);
+        router.use('/api/v1/directory', DirectoryRoutes.routes);
+        router.use('/api/v1/subdirectory', SubDirectoryRoutes.routes);
+        router.use('/api/v1/subdirectory-years', SubDirectoryYearRoutes.routes);
 
         return router;
     }
