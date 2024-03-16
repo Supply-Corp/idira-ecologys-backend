e4f9c0c56f3d137e7959d9ddcc532d37ec06e66c
zandrij <ajosuesh@gmail.com>
2024-03-16 17:42:46 -0500

    feat: new migrations directories

--- prisma/migrations/20240316224208_directories/migration.sql
+++ prisma/migrations/20240316224208_directories/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `Directory` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
+    `state` ENUM('ACTIVE', 'INACTIVE', 'DELETE') NOT NULL DEFAULT 'ACTIVE',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -13,6 +14,19 @@ CREATE TABLE `SubDirectory` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `directoryId` INTEGER NOT NULL,
+    `state` ENUM('ACTIVE', 'INACTIVE', 'DELETE') NOT NULL DEFAULT 'ACTIVE',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subdirectory_year` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `subDirectoryId` INTEGER NOT NULL,
+    `state` ENUM('ACTIVE', 'INACTIVE', 'DELETE') NOT NULL DEFAULT 'ACTIVE',
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -21,3 +35,6 @@ CREATE TABLE `SubDirectory` (
 
 -- AddForeignKey
 ALTER TABLE `SubDirectory` ADD CONSTRAINT `SubDirectory_directoryId_fkey` FOREIGN KEY (`directoryId`) REFERENCES `Directory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `subdirectory_year` ADD CONSTRAINT `subdirectory_year_subDirectoryId_fkey` FOREIGN KEY (`subDirectoryId`) REFERENCES `SubDirectory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
