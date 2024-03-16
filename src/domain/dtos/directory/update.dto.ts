a8f8d9815c6e4a7c2fbdb4238bcb93baf08c570f
zandrij <ajosuesh@gmail.com>
2024-03-16 17:46:11 -0500

    feat: update dtos (directories)

---
+++ src/domain/dtos/directory/update.dto.ts
@@ -0,0 +1,44 @@
+import { Schema } from "express-validator";
+import { FieldQuery } from "../../utils/field-query.util";
+import { FieldValidation } from "../../utils/field-validation.util";
+import { Request } from "express";
+
+export class UpdateDirectoryDto {
+    constructor(
+        public readonly id: number,
+        public readonly name: string
+    ) {}
+
+    private static schema: Schema = {
+        id: {
+            trim: true,
+            notEmpty: {
+                bail: true,
+                errorMessage: "Id es requerido"
+            },
+            isNumeric: {
+                bail: true,
+                errorMessage: "Id no es numérico"
+            }
+        },
+        name: {
+            trim: true,
+            notEmpty: {
+                bail: true,
+                errorMessage: "Nombre es requerido",
+            },
+        },
+    };
+
+    static async create(req: Request): Promise<[FieldQuery[]?, UpdateDirectoryDto?]> {
+        const { name } = req.body;
+        const id = req.params.id;
+
+        try {
+            const valid = await FieldValidation.validate(this.schema, req);
+            return [valid, undefined];
+        } catch (error) {
+            return [undefined, new UpdateDirectoryDto(+id,name)];
+        }
+    }
+}
