f50711954aa916e51fc60853a73707e012e3dc37
zandrij <ajosuesh@gmail.com>
2024-03-15 20:07:12 -0500

    feat: middleware token validation

--- src/presentation/middlewares/user-validation.middleware.ts
+++ src/presentation/middlewares/user-validation.middleware.ts
@@ -1,5 +1,7 @@
+import { NextFunction, Request, Response } from 'express';
 import { prisma } from '../../configuration/database/index';
-import { Roles } from '../../domain';
+import { Roles, UserEntity } from '../../domain';
+import { JwtAdapter } from '../../configuration';
 
 export class UserValidationMiddleware {
 
@@ -23,5 +25,30 @@ export class UserValidationMiddleware {
         if( valid.state === "DELETE") throw new Error("Sede no existe");
         
         return true;
+    }
+
+    static async auth (req: Request, res: Response, next: NextFunction) {
+        const authorization = req.header('Authorization');
+
+        if( !authorization ) return res.status(401).json({ error: 'Token not provided.' });
+        if( !authorization.startsWith('Bearer ')) return res.status(401).json({ error: 'Invalid Bearer token'});
+
+        const token = authorization.split(' ').at(1) || '';
+
+        try {
+            const payload = await JwtAdapter.validateToken<{ id: string }>( token );
+            if( !payload ) return res.status(401).json({ error: 'Invalid token' });
+
+            const user = await prisma.user.findFirst({ where: { id: +payload.id } });
+            if( !user ) return res.status(401).json({ error: 'Error token validation information.'});
+
+            const { password, ...auth } = UserEntity.fromObject( user );
+            req.body.user = auth;
+
+            next();
+        } catch (error) {
+            console.error( error );
+            return res.status(500).json({ error: 'Internal server error.'});
+        }
     }
 }
